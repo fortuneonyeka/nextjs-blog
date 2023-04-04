f7b685dfe887cdcefee6ed3b6b297382f089c08d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-04-04 05:27:45 +0100

    load third party script

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -11,7 +11,12 @@ const FirstPost = () => {
     <>
       <Head>
         <title>First Post</title>
-        
+        <Script src="https://connect.facebook.net/en_US/sdk.js" 
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+        />
       </Head>
       <div className={styles.container}>
 
